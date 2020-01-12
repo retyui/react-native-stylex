5fdd31532696674c86cc31d118bf28d92ae51449
David <4661784+retyui@users.noreply.github.com>
2020-01-12 17:57:41 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { ThemeProvider, ThemeConsumer } from "./context";
 export { default as useTheme } from "./useTheme";
 export { default as withStyles } from "./withStyles";
 export { default as makeUseStyles } from "./makeUseStyles";
+export { default as useColorTransition } from "./useColorTransition";
 export { maxWidth, maxHeight, minHeight, minWidth } from "./mediaQueryHelpers";
