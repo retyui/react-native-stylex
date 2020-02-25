69b5a8cb686a241d6c3989eb9833953094951894
David <4661784+retyui@users.noreply.github.com>
2020-02-25 22:08:29 +0300

    Add dependency control for react-native-dark-mode

---
+++ src/dark-mode.ts
@@ -0,0 +1,45 @@
+import {
+  eventEmitter,
+  initialMode,
+  useDarkModeContext
+} from "react-native-dark-mode";
+import { addDependency } from "./dependencyRegistry";
+
+const state = { mode: initialMode };
+
+eventEmitter.on("currentModeChanged", (newMode: string) => {
+  state.mode = newMode;
+});
+
+const DEPENDENCY_KEY = "react-native-dark-mode";
+
+addDependency(DEPENDENCY_KEY, () => useDarkModeContext());
+
+type InterfaceStyle<T> =
+  | {
+      dark: T;
+      light: T;
+    }
+  | {
+      dark: T;
+      light: undefined;
+    }
+  | {
+      dark: undefined;
+      light: T;
+    };
+
+export const uiMode = <T extends {}>({
+  dark,
+  light
+}: InterfaceStyle<T>): T | undefined => {
+  if (state.mode === "dark") {
+    return dark;
+  }
+
+  if (state.mode === "light") {
+    return light;
+  }
+
+  return undefined;
+};
